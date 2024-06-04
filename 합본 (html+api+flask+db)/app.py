0171c88e4aa8322a3112a62fd7f7c4a9eb7ab94a
hwayeonx <165234693+hwayeonx@users.noreply.github.com>
2024-06-04 21:06:37 +0900

    Add files via upload

---
+++ 합본 (html+api+flask+db)/app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
+from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
+from main import generate_summary
+from auto_web import download_pdf_and_extract_text
+
+app = Flask(__name__)
+app.secret_key = 'ninja1234'
+
+# MySQL configurations
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'mydatabase'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password)
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)", (username, email, hashed_password))
+        mysql.connection.commit()
+        cur.close()
+        flash('You are now registered and can log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE username = %s", [username])
+        user = cur.fetchone()
+        cur.close()
+
+        if user and check_password_hash(user[3], password):
+            flash('Login successful', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Invalid credentials', 'danger')
+    return render_template('login.html')
+
+@app.route('/main')
+def dashboard():
+    return render_template('main.html')
+
+@app.route('/university')
+def university():
+    return render_template('index.html')
+
+@app.route('/search', methods=["POST"])
+def search():
+    query = request.form["query"]
+    result = generate_summary(query)
+    return jsonify(result=result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
+
